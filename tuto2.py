abf27a8f63fe74f52b21ab5f20db87ed210f3637
Savino Lomuscio <francilomuscio@gmail.com>
2020-03-16 16:03:17 +0100

    Fix error on loading video from *.mp4 files

--- tuto2.py
+++ tuto2.py
@@ -24,6 +24,22 @@ while(capture.isOpened()):
 capture.release()
 cv2.destroyAllWindows()
 
+capture = cv2.VideoCapture("res/Gon's Anger.mp4")
+""" isOpened() controlla che la cattura sia sta inizializzata correttamente
+altrimenti si usa il metodo open() per forzare l'apertura """
+while(capture.isOpened()):
+    """ ret e' True quando il frame viene letto correttamente """
+    ret, frame = capture.read()
+    if(ret):
+        grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("grayscale camera", grayscale)
+        k = cv2.waitKey(1)
+        if k == ord('q'):
+            break
+        
+capture.release()
+cv2.destroyAllWindows()
+
 capture = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter("res/output.avi",fourcc,24,(640,480))
